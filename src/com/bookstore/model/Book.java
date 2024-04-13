56cef342e41eefc731a8285c265100d2402f9380
dvargas42 <dvargas.eng@gmail.com>
2024-04-12 22:03:51 -0300

    feat: :sparkles: add Book file

---
+++ src/com/bookstore/model/Book.java
@@ -0,0 +1,114 @@
+package com.bookstore.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity(name = "books")
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String title;
+	private String isbn;
+	private double price;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Calendar launchDate = Calendar.getInstance();
+	
+	@ManyToMany(fetch = FetchType.EAGER)	
+	private List<Author> authors = new ArrayList<Author>();
+
+
+	public List<Author> getAuthors() {
+		return this.authors;
+	}
+	
+	public void addAuthor(Author author) {
+		this.authors.add(author);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Calendar getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(Calendar launchDate) {
+		this.launchDate = launchDate;
+	}
+	
+	public void removeAuthor(Author author) {
+		this.authors.remove(author);
+		
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
